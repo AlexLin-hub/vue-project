d4bf506e15f9791a31d6cc15dde07890cf553c27
Uncle Lin <unclelin2020@gmail.com>
2021-11-20 11:15:01 +0800

    feat: default state

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,14 @@
 <template>
-  <hello-world />
+  <v-container>
+    <v-row>
+      <v-col><h1 class="text-center">HI! 歡迎你！</h1></v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  }
+export default {};
 </script>
+
+<style scoped>
+</style>
